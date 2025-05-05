5f96a052eae0e481221c3497e8af644e9dcd57e4
KusozakoAtama10k <64054297+KusozakoAtama10k@users.noreply.github.com>
2025-05-06 01:14:37 +0900

    Added wild crops

--- src/main/java/com/ka10k/umapyoidelight/item/ComposterRegistration.java
+++ src/main/java/com/ka10k/umapyoidelight/item/ComposterRegistration.java
@@ -11,6 +11,9 @@ public class ComposterRegistration {
         register(ItemRegistration.CHILI_ITEM, 0.65F);
         register(ItemRegistration.STRAWBERRY_ITEM, 0.65F);
         register(ItemRegistration.WHITE_BREAD, 0.65F);
+        register(ItemRegistration.WILD_CHILIS, 0.65F);
+        register(ItemRegistration.WILD_GARLIC, 0.65F);
+        register(ItemRegistration.WILD_STRAWBERRIES, 0.65F);
         register(ItemRegistration.GARLIC_ITEM_RARE, 0.85F);
         register(ItemRegistration.CHILI_ITEM_RARE, 0.85F);
         register(ItemRegistration.STRAWBERRY_ITEM_RARE, 0.85F);
